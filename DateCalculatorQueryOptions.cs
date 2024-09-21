361cbfa1dde1efc4c435ce66f5bb47f06ff4e2ed
APIVerve Team <hello@apiverve.com>
2024-09-21 01:08:12 -0500

    Bug Fixes

---
+++ DateCalculatorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DateCalculatorQueryOptions {
+public string start { get; set; }
+public string end { get; set; }
+}
+
+}
